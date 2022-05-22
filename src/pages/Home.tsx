8d9a45526ae342f21627a8865791489d7c237324
ssasmi <ssasmi@gmail.com>
2022-05-22 03:04:50 +0300

    complate

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
-import qs from 'qs';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import qs from "qs";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-import { Categories, Sort, PizzaBlock, Skeleton, Pagination } from '../components';
+import {
+  Categories,
+  Sort,
+  PizzaBlock,
+  Skeleton,
+  Pagination,
+} from "../components";
 
-import { sortList } from '../components/Sort';
+import { sortList } from "../components/Sort";
 
-import { useAppDispatch } from '../redux/store';
-import { selectFilter } from '../redux/filter/selectors';
-import { selectPizzaData } from '../redux/pizza/selectors';
-import { setCategoryId, setCurrentPage, setFilters } from '../redux/filter/slice';
-import { fetchPizzas } from '../redux/pizza/asyncActions';
-import { SearchPizzaParams } from '../redux/pizza/types';
+import { useAppDispatch } from "../redux/store";
+import { selectFilter } from "../redux/filter/selectors";
+import { selectPizzaData } from "../redux/pizza/selectors";
+import {
+  setCategoryId,
+  setCurrentPage,
+  setFilters,
+} from "../redux/filter/slice";
+import { fetchPizzas } from "../redux/pizza/asyncActions";
+import { SearchPizzaParams } from "../redux/pizza/types";
 
 const Home: React.FC = () => {
   const navigate = useNavigate();
@@ -20,7 +30,8 @@ const Home: React.FC = () => {
   const isMounted = React.useRef(false);
 
   const { items, status } = useSelector(selectPizzaData);
-  const { categoryId, sort, currentPage, searchValue } = useSelector(selectFilter);
+  const { categoryId, sort, currentPage, searchValue } =
+    useSelector(selectFilter);
 
   const onChangeCategory = React.useCallback((idx: number) => {
     dispatch(setCategoryId(idx));
@@ -31,9 +42,9 @@ const Home: React.FC = () => {
   };
 
   const getPizzas = async () => {
-    const sortBy = sort.sortProperty.replace('-', '');
-    const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
-    const category = categoryId > 0 ? String(categoryId) : '';
+    const sortBy = sort.sortProperty.replace("-", "");
+    const order = sort.sortProperty.includes("-") ? "asc" : "desc";
+    const category = categoryId > 0 ? String(categoryId) : "";
     const search = searchValue;
 
     dispatch(
@@ -43,7 +54,7 @@ const Home: React.FC = () => {
         category,
         search,
         currentPage: String(currentPage),
-      }),
+      })
     );
 
     window.scrollTo(0, 0);
@@ -63,7 +74,9 @@ const Home: React.FC = () => {
       navigate(`/?${queryString}`);
     }
 
-    const params = qs.parse(window.location.search.substring(1)) as unknown as SearchPizzaParams;
+    const params = qs.parse(
+      window.location.search.substring(1)
+    ) as unknown as SearchPizzaParams;
     const sortObj = sortList.find((obj) => obj.sortProperty === params.sortBy);
     dispatch(
       setFilters({
@@ -71,7 +84,7 @@ const Home: React.FC = () => {
         categoryId: Number(params.category),
         currentPage: Number(params.currentPage),
         sort: sortObj || sortList[0],
-      }),
+      })
     );
 
     getPizzas();
@@ -96,7 +109,9 @@ const Home: React.FC = () => {
   // }, []);
 
   const pizzas = items.map((obj: any) => <PizzaBlock key={obj.id} {...obj} />);
-  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
+  const skeletons = [...new Array(6)].map((_, index) => (
+    <Skeleton key={index} />
+  ));
 
   return (
     <div className="container">
@@ -105,13 +120,18 @@ const Home: React.FC = () => {
         <Sort value={sort} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      {status === 'error' ? (
+      {status === "error" ? (
         <div className="content__error-info">
           <h2>Произошла ошибка 😕</h2>
-          <p>К сожалению, не удалось получить питсы. Попробуйте повторить попытку позже.</p>
+          <p>
+            К сожалению, не удалось получить питсы. Попробуйте повторить попытку
+            позже.
+          </p>
         </div>
       ) : (
-        <div className="content__items">{status === 'loading' ? skeletons : pizzas}</div>
+        <div className="content__items">
+          {status === "loading" ? skeletons : pizzas}
+        </div>
       )}
 
       <Pagination currentPage={currentPage} onChangePage={onChangePage} />
